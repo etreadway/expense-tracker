1d413e0e3b4a36175bdb1b1128450ee105205403
Eric Treadway <eric.s.treadway@gmail.com>
2021-08-04 14:37:32 -0500

    made ExpenseItem ready for props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 7, 4);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>August 4th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
