9bac9730441c1f0c16227e1421bbbb184e418790
Eric Treadway <eric.s.treadway@gmail.com>
2021-08-06 19:01:29 -0500

    added styling lable to the expenses component

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div>
+    <div className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
